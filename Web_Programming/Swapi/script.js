41007125938ed905a94d0b557aab21644a3d576b
Julian Marquardt <117978569+Julian-wi22060@users.noreply.github.com>
2023-09-25 15:25:35 +0200

    new script

--- Web_Programming/Swapi/script.js
+++ Web_Programming/Swapi/script.js
@@ -1,22 +1,35 @@
 async function zeigeZufallsCharakter() {
-    const charakterId = Math.floor(Math.random() * 82) + 1; // Zufällige Charakter-ID zwischen 1 und 82
-    const charakter = await ladeCharakter(charakterId);
-  
-    if (charakter) {
-      document.getElementById('charakterName').textContent = charakter.name;
-      document.getElementById('charakterAugenfarbe').textContent = charakter.eye_color;
-      document.getElementById('charakterHeimatwelt').textContent = charakter.homeworld;
-    }
+  const charakterId = Math.floor(Math.random() * 82) + 1; // Zufällige Charakter-ID zwischen 1 und 82
+  const charakter = await ladeCharakter(charakterId);
+
+  if (charakter) {
+    document.getElementById('charakterName').textContent = charakter.name;
+    document.getElementById('charakterAugenfarbe').textContent = charakter.eye_color;
+
+    // Heimatzeitwelt laden und anzeigen
+    const heimatzeitwelt = await ladeHeimatzeitwelt(charakter.homeworld);
+    document.getElementById('charakterHeimatzeitwelt').textContent = heimatzeitwelt;
   }
-  
-  async function ladeCharakter(charakterId) {
-    try {
-      const response = await fetch(`https://swapi.dev/api/people/${charakterId}/`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Fehler beim Laden des Charakters:', error);
-      return null;
-    }
+}
+
+async function ladeCharakter(charakterId) {
+  try {
+    const response = await fetch(`https://swapi.dev/api/people/${charakterId}/`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fehler beim Laden des Charakters:', error);
+    return null;
   }
-  
+}
+
+async function ladeHeimatzeitwelt(heimatzeitweltUrl) {
+  try {
+    const response = await fetch(heimatzeitweltUrl);
+    const data = await response.json();
+    return data.name;
+  } catch (error) {
+    console.error('Fehler beim Laden der Heimatzeitwelt:', error);
+    return 'Unbekannte Heimatzeitwelt';
+  }
+}
